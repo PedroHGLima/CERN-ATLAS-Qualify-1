04668e2e0d0e4c9270dec3fc6a0b23fd63896352
Natanael Moura-Junior <natmourajr@gmail.com>
2021-08-06 20:02:26 -0300

    adding ringer-jr image

---
+++ docker_images/ringer-jr/README.md
@@ -0,0 +1,7 @@
+# CERN-ATLAS-Qualify/docker_images/images
+Aqui temos as imagens propriamente ditas. Esta pasta deve estar vazia no github (espaço do repositório).
+
+## Criação de uma imagem
+```console
+docker build --file base/Dockerfile ./images/
+```
---
+++ docker_images/ringer-jr/setup_all_here.sh
@@ -0,0 +1,53 @@
+
+github_username=$1
+
+
+echo "======================================================================================"
+echo "setup root..."
+# Set ROOT by hand
+export ROOTSYS="/opt/root/buildthis"
+export PATH="$ROOTSYS/bin:$PATH"
+export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="/opt/root/buildthis/lib:$PYTHONPATH"
+
+echo "======================================================================================"
+echo "setup gaugi..."
+current=$PWD
+cd $(mktemp -d)
+mkdir git_repos
+cd git_repos
+temp=$PWD
+
+cd $temp && git clone https://github.com/$github_username/gaugi.git
+cd $temp/gaugi && source scripts/setup.sh
+echo "======================================================================================"
+echo "setup prometheus..."
+cd $temp && git clone https://github.com/$github_username/prometheus.git
+cd $temp/prometheus && mkdir build && cd build && cmake .. && make && cd .. && source setup.sh
+
+echo "======================================================================================"
+echo "setup kolmov..."
+cd $temp && git clone https://github.com/$github_username/kolmov.git
+cd $temp/kolmov && source scripts/setup.sh
+
+echo "======================================================================================"
+echo "setup saphyra..."
+cd $temp && git clone https://github.com/$github_username/saphyra.git
+cd $temp/saphyra && source scripts/setup.sh
+
+
+echo "======================================================================================"
+echo "setup orchestra..."
+cd $temp && git clone https://github.com/$github_username/orchestra.git
+cd $temp/orchestra && source setup.sh
+
+
+echo "======================================================================================"
+echo "setup tunings..."
+cd $temp && git clone https://github.com/$github_username/ringer_tunings.git
+cd $temp/ringer_tunings && source setup.sh
+
+echo "======================================================================================"
+echo "all packages can be found at $temp."
+echo "======================================================================================"
+cd $current
